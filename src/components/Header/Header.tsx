02c3e799c9d912abcb1ffdd9c02c88a0a2353697
Oleg Filatov <oleg.filatov@gs-labs.ru>
2020-11-10 15:41:35 +0300

    added router hooks and cards to pokedex page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
 import s from './Header.module.scss';
-import {ReactComponent as PokemonLogoSvg} from './assets/img/pokemon-logo.svg';
+import { ReactComponent as PokemonLogoSvg } from './assets/img/pokemon-logo.svg';
 import Menu from './Menu/Menu';
+import { GeneralMenu } from '../../routes';
 
 const Header = () => (
   <div className={s.container}>
-    <PokemonLogoSvg className={s.logo}/>
-    <Menu menuElements={[
-        { value: 'Home',link: '#' },
-        { value: 'Pokédex',link: '#' },
-        { value: 'Legendaries',link: '#' },
-        { value: 'Documentation',link: '#' }
-       ]}/>
+    <PokemonLogoSvg className={s.logo} />
+    <Menu menuElements={GeneralMenu} />
   </div>
 );
 
--- src/components/Header/Menu/Menu.tsx
+++ src/components/Header/Menu/Menu.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react';
+import { A, usePath } from 'hookrouter';
+import cn from 'classnames';
 import s from '../Header.module.scss';
 
 interface Props {
-  menuElements: Array<{value: string, link: string}>;
+  menuElements: Array<{ title: string; link: string }>;
 }
 
 const Menu = ({ menuElements }: Props) => {
-  const menuList = menuElements.map(({value,link}) => (
-    <div key={value} className={s.menuElement}>
-      <a href={link} className={s.menuLink}>
-        {value}
-      </a>
+  const path = usePath();
+  const menuList = menuElements.map(({ title, link }) => (
+    <div key={title} className={s.menuElement}>
+      <A
+        href={link}
+        className={cn(s.menuLink, {
+          [s.activeLink]: link === path,
+        })}>
+        {title}
+      </A>
     </div>
   ));
   return <nav className={s.menu}>{menuList}</nav>;
