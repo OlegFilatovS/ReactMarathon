54f95d8ae01046f52eecb780522d0a84cd983725
Oleg Filatov <oleg.filatov@gs-labs.ru>
2020-11-18 22:31:07 +0300

    added req,getDataHook and simple input to Pokedex

--- src/Pages/Pokedex/Pokedex.tsx
+++ src/Pages/Pokedex/Pokedex.tsx
@@ -1,68 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import s from './Pokedex.module.scss';
 import PokemonCard from '../../components/PokemonCard';
 import Layout from '../../components/Layout';
-
-const usePokemons = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    const getPokemons = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch('http://zar.hosthot.ru/api/v1/pokemons');
-        const result = await response.json();
-        setData(result);
-      } catch (e) {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    getPokemons();
-  }, []);
-
-  return {
-    data,
-    isLoading,
-    isError,
-  };
-};
+import Heading from '../../components/Heading';
+import useData from '../../hook/getData';
 
 const Pokedex = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const [query, setQuery] = useState({});
+
   const {
     data: { total, pokemons },
     isLoading,
     isError,
-  } = usePokemons();
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  } = useData('getPokemons', query, [searchValue]);
 
   if (isError) {
     return <div>Error...</div>;
   }
 
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+    setQuery((s) => ({
+      ...s,
+      name: e.target.value,
+    }));
+  };
+
   return (
     <div className={s.root}>
       <Layout className={s.contentWrap}>
-        <div className={s.title}>
-          {total} <span className={s.titleHeavy}>Pokemons</span> for you to choose your favorite
+        <Heading headingLevel={1} className={s.title}>
+          {!isLoading && total} <span className={s.titleHeavy}>Pokemons</span> for you to choose your favorite
+        </Heading>
+        <div>
+          <input type="text" value={searchValue} onChange={handleSearchChange} />
         </div>
         <div className={s.cardsWrapper}>
-          {pokemons.map((pokemon) => (
-            <PokemonCard
-              pokemonName={pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
-              attackValue={pokemon.stats.attack}
-              defenseValue={pokemon.stats.defense}
-              pokemonType={pokemon.types?.[0]}
-              pokemonImageSrc={pokemon.img}
-            />
-          ))}
+          {!isLoading &&
+            pokemons.map((pokemon) => (
+              <PokemonCard
+                pokemonName={pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                attackValue={pokemon.stats.attack}
+                defenseValue={pokemon.stats.defense}
+                pokemonType={pokemon.types?.[0]}
+                pokemonImageSrc={pokemon.img}
+              />
+            ))}
         </div>
       </Layout>
     </div>
